d26a5d4675c6bc139d7fb4a7de3e603b3de99ce2
Joanna Kao <joannaskao@gmail.com>
2014-08-04 08:42:08 -0400

    added/deleted necessary osx apps

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -1,4 +1,6 @@
 # Sort alphabetically by name
 node.default["sublime_text_packages"] = [
-  {"name" => "Theme - Soda", "source" => "https://github.com/buymeasoda/soda-theme"}
+  {"name" => "Theme - Soda", "source" => "https://github.com/buymeasoda/soda-theme"},
+  {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
+  {"name" => "Jade", "source" => "https://github.com/davidrios/jade-tmbundle"}
 ]
