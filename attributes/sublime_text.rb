27d8f77cb11abd0df954aa7dc11f2ccd80987fa4
Joanna Kao <joannaskao@gmail.com>
2014-08-04 11:35:00 -0400

    added more sublime text packages from michael

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -2,5 +2,8 @@
 node.default["sublime_text_packages"] = [
   {"name" => "Theme - Soda", "source" => "https://github.com/buymeasoda/soda-theme"},
   {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
-  {"name" => "Jade", "source" => "https://github.com/davidrios/jade-tmbundle"}
+  {"name" => "Jade", "source" => "https://github.com/davidrios/jade-tmbundle"},
+  {"name" => "Stylus", "source" => "https://github.com/billymoon/Stylus"},
+  {"name" => "AdvancedNewFile", "source" => "https://github.com/skuroda/Sublime-AdvancedNewFile"},
+  {"name" => "CSS Color Converter", "source" => "https://github.com/TheDutchCoder/ColorConvert"},
 ]
