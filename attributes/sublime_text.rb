accee4d342ab3ee0dce13874597ed86561d2fb31
Joanna Kao <joannaskao@gmail.com>
2014-08-04 16:43:10 -0400

    tweaks with sublime. delete s3cmd attribute (not installing)

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -1,6 +1,5 @@
 # Sort alphabetically by name
 node.default["sublime_text_packages"] = [
-  {"name" => "Theme - Soda", "source" => "https://github.com/buymeasoda/soda-theme"},
   {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
   {"name" => "Jade", "source" => "https://github.com/davidrios/jade-tmbundle"},
   {"name" => "Stylus", "source" => "https://github.com/billymoon/Stylus"},
