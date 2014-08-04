86a7fb3df0083859796d54698ba623dd438076ad
Joanna Kao <joannaskao@gmail.com>
2014-08-04 12:05:35 -0400

    add jinja syntax highlighting package

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -6,5 +6,6 @@ node.default["sublime_text_packages"] = [
   {"name" => "Stylus", "source" => "https://github.com/billymoon/Stylus"},
   {"name" => "AdvancedNewFile", "source" => "https://github.com/skuroda/Sublime-AdvancedNewFile"},
   {"name" => "CSS Color Converter", "source" => "https://github.com/TheDutchCoder/ColorConvert"},
-  {"name" => "SublimePrettyJson", "source" => "https://github.com/dzhibas/SublimePrettyJson"}
+  {"name" => "SublimePrettyJson", "source" => "https://github.com/dzhibas/SublimePrettyJson"},
+  {"name" => "Jinja2 Syntax Highlighting", "source" => "https://github.com/mitsuhiko/jinja2-tmbundle"}
 ]
