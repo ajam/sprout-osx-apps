7abe83064b11f84d2e20c6e3c6bdac33ce0395a2
Joanna Kao <joannaskao@gmail.com>
2014-08-06 18:03:10 -0400

    remove volumes_dir attribute

--- recipes/google_voice_and_video.rb
+++ recipes/google_voice_and_video.rb
@@ -5,7 +5,6 @@ dmg_package 'Google Voice and Video' do
   checksum dmg_properties['checksum']
   type 'pkg'
   package_id dmg_properties['package_id']
-  volumes_dir dmg_properties['volumes_dir']
   action :install
   owner node['current_user']
 end
