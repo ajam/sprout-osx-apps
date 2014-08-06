1378e71e3c48d94b027af230ac5ca650a293a953
Joanna Kao <joannaskao@gmail.com>
2014-08-06 19:22:26 -0400

    modify brew script again

--- recipes/scripts-brew.rb
+++ recipes/scripts-brew.rb
@@ -1,7 +1,6 @@
-bash "install_brew_formulas" do
+bash "install_qgis-24" do
   cwd "~/"
   code <<-EOH
-  	brew install https://raw.github.com/osgeo/homebrew-osgeo4mac/master/qgis-24.rb
+  	brew install https://raw.github.com/osgeo/homebrew-osgeo4mac/master/qgis-24.rb"
   EOH
-  action :run 
 end
