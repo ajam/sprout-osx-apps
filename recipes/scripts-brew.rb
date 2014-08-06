d9b0e2b15e7e978ac05439144709a37852f00f6d
Joanna Kao <joannaskao@gmail.com>
2014-08-06 19:17:14 -0400

    modified brew formula

--- recipes/scripts-brew.rb
+++ recipes/scripts-brew.rb
@@ -1,8 +1,7 @@
-script "install_brew_formulas" do
-  interpreter "bash"
-  user "root"
-  cwd "/tmp"
+bash "install_brew_formulas" do
+  cwd "~/"
   code <<-EOH
   	brew install https://raw.github.com/osgeo/homebrew-osgeo4mac/master/qgis-24.rb
   EOH
+  action :run 
 end
