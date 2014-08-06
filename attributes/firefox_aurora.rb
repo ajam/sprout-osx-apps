e16dae3923fdd794d379ed331a1caa3510253276
Joanna Kao <joannaskao@gmail.com>
2014-08-06 17:30:13 -0400

    updated source for latest firefox aurora

--- attributes/firefox_aurora.rb
+++ attributes/firefox_aurora.rb
@@ -1,3 +1,4 @@
 default['sprout']['firefox_aurora']['dmg']['app']         = 'FirefoxAurora'
-default['sprout']['firefox_aurora']['dmg']['source']      = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-34.0a1.en-US.mac.dmg'
+default['sprout']['firefox_aurora']['dmg']['checksum']    = '03987ac962b6b1eb8e1f89b287973db052f58364'
+default['sprout']['firefox_aurora']['dmg']['source']      = 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-33.0a2.en-US.mac.dmg'
 default['sprout']['firefox_aurora']['dmg']['volumes_dir'] = 'Aurora'
