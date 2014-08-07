3f7802241056dedbadc2aad984121ec774c50572
Joanna Kao <joannaskao@gmail.com>
2014-08-06 21:05:16 -0400

    fixing formula name

--- recipes/scripts-brew.rb
+++ recipes/scripts-brew.rb
@@ -1,6 +1,6 @@
 bash "install_qgis-24" do
   code <<-EOH
   brew tap osgeo/osgeo4mac
-  brew install https://raw.githubusercontent.com/OSGeo/homebrew-osgeo4mac/master/Formula/qgis-24.rb
+  brew install qgis-24.rb
   EOH
 end
